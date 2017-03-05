a60fda6ec64742feb26ad1198013ff5da71bb8d1
xiangs <xian0009@e.ntu.edu.sg>
2017-03-05 13:47:46 +0800

    Update 3

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+import views
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
